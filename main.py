16a3d98d9b455fd857acb079006eb1f68d6f9097
TubsKrt <103363656+TubsKrt@users.noreply.github.com>
2026-03-23 11:36:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ plt.ylabel("Perceived Time")
 plt.title("Time Perception Bias")
 
 plt.show()
+
+
+import matplotlib.pyplot as plt
+
+# Beispiel Daten
+real_time = [10, 10, 10]
+perceived_time = [14, 8, 13]
+
+# Diagramm
+plt.plot(real_time, label="Real Time")
+plt.plot(perceived_time, label="Perceived Time")
+
+plt.title("Time Perception Comparison")
+plt.xlabel("Participants")
+plt.ylabel("Time")
+plt.legend()
+
+plt.show()
